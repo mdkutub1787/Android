f06a5cfe51338835e362bb2b3850747645257a3c
Md Kutub Uddin <mdkutub150@gmail.com>
2024-10-04 20:20:58 +0600

    add

--- AndroidJavaApp/app/src/main/java/com/kutub/androidjavaapp/MainActivity.java
+++ AndroidJavaApp/app/src/main/java/com/kutub/androidjavaapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.kutub.androidjavaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnHome=findViewById(R.id.btnHome);
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+            }
         });
     }
 }
