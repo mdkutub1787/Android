04dc53cf0f35415c1dece35526258c5b4d0dac36
Md Kutub Uddin <mdkutub150@gmail.com>
2024-10-05 11:56:06 +0600

    add

--- InsuranceApp/app/src/main/java/com/kutub/insuranceapp/Home.java
+++ InsuranceApp/app/src/main/java/com/kutub/insuranceapp/Home.java
@@ -1,6 +1,13 @@
 package com.kutub.insuranceapp;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +16,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Home extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,32 @@ public class Home extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        TextView textView = findViewById(R.id.textView2);
+        EditText editTextName = findViewById(R.id.editTextName);
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        EditText editTextPhone = findViewById(R.id.editTextPhone);
+        RadioGroup radioGroupGender = findViewById(R.id.radioGroupGender);
+        Button buttonSubmit = findViewById(R.id.buttonSubmit);
+
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editTextName.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String phone = editTextPhone.getText().toString();
+
+               
+                int selectedGenderId = radioGroupGender.getCheckedRadioButtonId();
+                RadioButton selectedGender = findViewById(selectedGenderId);
+                String gender = selectedGender != null ? selectedGender.getText().toString() : "Not Selected";
+
+
+                textView.setText("Name: " + name + "\nEmail: " + email + "\nPhone: " + phone + "\nGender: " + gender);
+            }
+        });
+
     }
 }
