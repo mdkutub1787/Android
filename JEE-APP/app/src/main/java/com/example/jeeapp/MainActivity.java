3b689bff26be162151cd3f924a0690f646ddf1bd
Md Kutub Uddin <mdkutub150@gmail.com>
2024-10-06 12:33:33 +0600

    add

--- JEE-APP/app/src/main/java/com/example/jeeapp/MainActivity.java
+++ JEE-APP/app/src/main/java/com/example/jeeapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.jeeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Animation zoomIn= AnimationUtils.loadAnimation(this,R.anim.zoom_in);
+
+        ImageView logo=findViewById(R.id.logo);
+
+       logo.setAnimation(zoomIn);
+
+       zoomIn.setAnimationListener(new Animation.AnimationListener() {
+           @Override
+           public void onAnimationStart(Animation animation) {
+
+           }
+
+           @Override
+           public void onAnimationEnd(Animation animation) {
+               Intent intent=new Intent(getApplicationContext(), Home.class);
+               startActivity(intent);
+               finish();
+           }
+
+           @Override
+           public void onAnimationRepeat(Animation animation) {
+
+           }
+       });
+
+
     }
 }
