4a09e13fd37951b508b55ea9be3f54662aae1579
Md Kutub Uddin <mdkutub150@gmail.com>
2024-10-10 12:36:43 +0600

    add

--- Java-Project-FB/app/src/main/java/com/example/javaprojectfb/MainActivity.java
+++ Java-Project-FB/app/src/main/java/com/example/javaprojectfb/MainActivity.java
@@ -4,37 +4,85 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
     private Button btnSave;
+
+    private TextView textView;
+    private Button btnShow;
     private DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        editText=findViewById(R.id.editText);
-        btnSave=findViewById(R.id.btnSave);
+        editText = findViewById(R.id.editText);
+        btnSave = findViewById(R.id.btnSave);
 
-        databaseReference= FirebaseDatabase.getInstance().getReference().child("MyDB");
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("MyDB");
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String data= editText.getText().toString();
-                databaseReference.setValue(data);
+                String data = editText.getText().toString();
+                databaseReference.setValue(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(MainActivity.this, "Data Add Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Data Add Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        textView = findViewById(R.id.textView);
+        btnShow = findViewById(R.id.btnShow);
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()){
+                            String showdata=snapshot.getValue().toString();
+                            textView.setText(showdata);
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "No Data ", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
         });
     }
