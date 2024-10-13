8adc8f432b8fa86eb76c668e85640570c969fe8c
Md Kutub Uddin <mdkutub150@gmail.com>
2024-10-13 15:20:29 +0600

    add

--- Insurance-Crud/app/src/main/java/com/kutub/insurancecrud/MainActivity.java
+++ Insurance-Crud/app/src/main/java/com/kutub/insurancecrud/MainActivity.java
@@ -1,24 +1,32 @@
- package com.kutub.insurancecrud;
+package com.kutub.insurancecrud;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        fab = findViewById(R.id.fab);
+
+        // Floating action button click listener
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Save.class);
+                startActivity(intent);
+            }
         });
     }
 }
