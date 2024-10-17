a9db836f3cdac46fa4dac151c87411fc7d10e2bc
Md Kutub Uddin <mdkutub150@gmail.com>
2024-10-17 23:57:54 +0600

    add

--- Blood_And_Buddies/app/src/main/java/com/kutub/bloodandbuddies/MainActivity.java
+++ Blood_And_Buddies/app/src/main/java/com/kutub/bloodandbuddies/MainActivity.java
@@ -1,24 +1,44 @@
 package com.kutub.bloodandbuddies;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    private TextView title, slogan;
+
+    Animation topAnimation, bottomAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        logo = findViewById(R.id.logo);
+        title = findViewById(R.id.title);
+        slogan = findViewById(R.id.slogan);
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.to_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        logo.setAnimation(topAnimation);
+        title.setAnimation(bottomAnimation);
+        slogan.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 3000);
     }
 }
